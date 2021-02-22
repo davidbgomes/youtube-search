b89c20400b08f42b01dd89683efb934dd32c4732
David Gomes <davidbgomes92@gmail.com>
2021-02-22 13:17:49 +0000

    changes in server to use dotenv

--- server/Server.js
+++ server/Server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express = require('express')
 var axios = require('axios')
 var cors = require('cors')
@@ -7,7 +8,7 @@ const port = 5000
 
 app.get('/search', (req, res) => {
 
-	let url = `https://www.googleapis.com/youtube/v3/search?key=${secrets.API_KEY}&type=video&part=snippet&maxResults=9&q=`
+	let url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.API_KEY}&type=video&part=snippet&maxResults=9&q=`
 
 	console.log("params",req.query.searchText)
 
